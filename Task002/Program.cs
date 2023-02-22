94658903567c1b4b552e18e21323e0ee557d9f38
DmitriyKuznecov-azur <kuznecov.d@azurgames.com>
2023-02-22 14:33:05 +0300

    solution of the second problem

---
+++ Task002/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+int InputUserNumber() // Метод Получения числа 
+{
+    Console.Write($"Введите число  --> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigitNumbers(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        int digit = num % 10;
+        num /= 10;
+        result += digit;
+    }
+    return result;
+}
+
+int userNumber = InputUserNumber();
+Console.WriteLine($"В числе {userNumber} сумма цифр = {SumDigitNumbers(userNumber)}");
